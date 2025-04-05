404aebda1c01900f827a0e47bee82f7ba70f0417
sergio lopez loureiro <lopez.loureiro.sergio.ald@gmail.com>
2025-04-05 17:04:21 +0200

    chore: First commit - Portfolio React and Tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+      <nav className="p-4 bg-white shadow-md">
+        <h1 className="text-xl font-bold">Mi Portfolio</h1>
+      </nav>
+    )
+  }
+  
+  export default Navbar
+  
